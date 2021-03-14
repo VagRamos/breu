0034ba3e6cbbe3e660af9d6b80f50d1a978d0b2e
VagRamos <vag.pare@gmail.com>
2021-03-13 22:59:05 -0300

    continuando layout moblie-first, menu e banner.

--- project/index.php
+++ project/index.php
@@ -5,22 +5,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Breu Metal</title>
+    <meta name="description" content="Banda de Thrash e Metal de Francisco Morato-SP">
+    <meta name="keywords" content="Breu Metal, Breu, Rock, Morato, Rock Francisco Morato">
+    <meta name="robots" content="index, follow">
+    <meta name="author" content="vagner Ramos">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lacquer&family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="#img/breu-icon.png"> 
 </head>
 <body>
 
     <header class="cabecalho">
-        <a href="index.php"><h1>LOGO DA BANDA</h1></a>  <!-- Logo da banda -->
-        <button class="btn-menu"><i class="fa fa-bars fa-lg"></i></button>
+        <a href="index.php"><h1 class="logo">Breu Metal</h1></a>
+         <button class="btn-menu"><i class="fa fa-bars fa-lg"></i></button>
         <nav class="menu">
-            <a class="close-btn">x</a>
+            <a class="btn-close"><i class="fa fa-times"></i></a>
             <ul>
                 <li><a class="" href="#">Home</a></li>
-                <li><a class="" href="#">Breu</a></li>
+                <li><a class="" href="#">Que Breu?</a></li>
                 <li><a class="" href="#">Vídeos</a></li>
                 <li><a class="" href="#">Contato</a></li>
             </ul>
         </nav>
     </header>
+    <div class="banner">
+        <div class="caixa">
+            <!-- <h2> BREU Metal!</h2>
+            <h3>Banda originária da "província" de Francisco Morato,   
+                um município esquecido de São Paulo, exceto quando das trágedias
+                que vendem boas matérias e dão audiência às mídias oportunistas.</h3> -->
+        </div>
+    </div>
 
     <div>
         <p>Banda originária da "província" de Francisco Morato,   
@@ -28,7 +45,7 @@
         <br/>que vendem boas matérias e dão audiência às mídias oportunistas.</p> 
     </div>
 
-    <div>
+    <div class="contato">
     <!-- terminar formulário -->
         <form action=""> 
             <div>
@@ -45,9 +62,19 @@
             </div>
         </form>
     </div>
+
     <footer>
         <p>2021 &copy; BREU Metal. Todos os direitos reservados.</p>
     </footer>
-  
+
+<script src="https://code.jquery.com/jquery-3.0.0.js"></script>
+<script>
+    $(".btn-menu").click(function(){
+        $(".menu").show();
+    });
+    $(".btn-close").click(function(){
+        $(".menu").hide();
+    });
+</script>
 </body>
 </html>
